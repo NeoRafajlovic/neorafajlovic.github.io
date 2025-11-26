920a9cad1835cfd0f2ca7df2313b8d132cd7ed69
Neo Rafajlovic <97417002+NeoRafajlovic@users.noreply.github.com>
2025-11-26 15:26:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const trailer = document.getElementById("trailer");
+const body = document.querySelector("body");
+
+body.addEventListener("mouseover", function () {
+  trailer.style.opacity = 1;
+});
+
+body.addEventListener("mouseout", function () {
+  trailer.style.opacity = 0;
+});
 
 window.onmousemove = (e) => {
   const x = e.clientX - trailer.offsetWidth / 2,
     y = e.clientY - trailer.offsetHeight / 2;
 
   trailer.style.transform = `translate(${x}px, ${y}px)`;
+};
+
+window.onclick = (e) => {
+  trailer.style.opacity = 0;
+  setTimeout(() => {
+    trailer.style.opacity = 1;
+  }, 150);
 };
