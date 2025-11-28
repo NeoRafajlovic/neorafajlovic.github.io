5721473013c59dacb6336def1270f398bc400d03
Neo Rafajlovic <97417002+NeoRafajlovic@users.noreply.github.com>
2025-11-27 22:22:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const trailer = document.getElementById("trailer");
 const body = document.querySelector("body");
+const navbar = document.getElementById("navbar");
 
 body.addEventListener("mouseover", function () {
-  trailer.style.opacity = 1;
+  trailer.style.opacity = 0.75;
 });
 
 body.addEventListener("mouseout", function () {
@@ -15,9 +16,38 @@ window.onmousemove = (e) => {
   trailer.style.transform = `translate(${x}px, ${y}px)`;
 };
 
-window.onclick = (e) => {
-  trailer.style.opacity = 0.3;
+let mouseIsUp = false;
+let cursorIsDown = false;
+
+window.addEventListener("mouseup", (e) => {
+  mouseIsUp = true;
+  if (cursorIsDown) {
+    trailer.style.opacity = 0.75;
+    cursorIsDown = false;
+  }
+});
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY >= 50) {
+    navbar.style.height = "50px";
+    navbar.style.opacity = 0.5;
+    navbar.style.backgroundColor = "rgb(50, 50, 50)";
+  } else {
+    navbar.style.height = "150px";
+    navbar.style.opacity = 0.75;
+    navbar.style.backgroundColor = "rgb(20, 20, 20)";
+  }
+});
+
+window.onmousedown = (e) => {
+  mouseIsUp = false;
+  trailer.style.opacity = 0;
   setTimeout(() => {
-    trailer.style.opacity = 1;
+    if (mouseIsUp) {
+      trailer.style.opacity = 0.75;
+      cursorIsDown = false;
+    } else {
+      cursorIsDown = true;
+    }
   }, 150);
 };
